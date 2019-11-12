c0b84da891b92fb4c8da416285e02955b29f61f7
Rose Seraphin <roseseraphin@yahoo.com>
2019-11-12 02:50:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def line 
+  
+  if line == 0 
+    puts "The line is currently empty."
+    
+  else 
+    
+end 
+end 
+
+def take_a_number(deli, name)
+  deli.length 
+  puts    #puts the person name with their position in line
+end 
+
+def now_serving
+  
+end 
+
+
+
+
+#1....build line method to show everyone's current place in line - if nobody in line display "The line is currently empty."
